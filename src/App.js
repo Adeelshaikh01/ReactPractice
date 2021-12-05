88530f450b0c59944ec9c5a2caab390a621a9f08
Adeelshaikh01 <adeelshaikh654@gmail.com>
2021-12-06 00:13:30 +0500

    theme added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Button, Input } from './components';
-import {useState} from 'react';
+import {useState,createContext} from 'react';
 
-
+export const GlobalTheme = createContext()
 function App() {
   const [value, setValue] = useState("");
   const [todos, setTodos] = useState([]);
+  const [themeDark, setTheme] = useState(false);
+  
+  
   const addTodo = () => {
     let arr = [...todos];
     arr.push(value)
@@ -17,13 +20,18 @@ function App() {
     let arr = [...todos];
     arr.splice(i, 1)
     setTodos(arr)
+    console.log("das")
   }
 
   const deleteAll =()=>{
     setTodos([])
   }
   
+  const themeChange = ()=>{
+    themeDark === false ? setTheme(true) : setTheme(false)
+  }
   return (
+    <GlobalTheme.Provider value={themeDark}>
     <div>
       <Input value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="Enter value" />
       <Button onClick={addTodo}>Add</Button>
@@ -33,7 +41,10 @@ function App() {
           return <li key={i}>{v} <Button onClick={() => deleteTodo(i)}>Delete</Button></li>
         })}
       </ul>
+      {themeDark ? <p>Dark</p> : <p>Light</p>}
+      <button onClick={themeChange}>change theme</button>
     </div>
+    </GlobalTheme.Provider>
   )
 }
 
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
+import { GlobalTheme } from '../../App';
 import './css/button.css'
 
 function Button(props) {
+    const theme = useContext(GlobalTheme)
+    
     let {onClick,children } = props;
     return (
-        <button className="btn" onClick={onClick}>{children}</button>
+        <button className={`btn ${theme ? "btnLight" : "btnDark"}`} onClick={onClick}>{children}</button>
         )
 }
 
